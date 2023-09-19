02d05013a31fe7918ae978a1e239b60848308c39
tolu <toluadegbilero@gmail.com>
2023-09-19 18:55:10 -0400

    Added from school laptop

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Keeey'
+app_name = 'myapp'
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route("/")
+def homepage():
+    return render_template('home.html')
+
+
+@app.route('/syllabus')
+def index_page():
+    return render_template('syllabus.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
