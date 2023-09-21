4709f485c2f22f1bf2ba1d492d4d5e43feec49b5
tolu <toluadegbilero@gmail.com>
2023-09-20 21:28:59 -0400

    Article additions

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ app_name = 'myapp'
 def homepage():
     return render_template('home.html')
 
+@app.route('/article_home')
+def article_homepage():
+    return render_template('Articles/article_home.html')
 
 @app.route('/syllabus')
 def index_page():
     return render_template('syllabus.html')
+
+@app.route("/Articles/Unit1/u1.1")
+def u11_page():
+    return render_template('u1.1.html')
 
 
 if __name__ == '__main__':
