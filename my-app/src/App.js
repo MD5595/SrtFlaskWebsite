18effed01e92d77241a86d79025a14568a0642ac
tolu <toluadegbilero@gmail.com>
2023-10-23 21:08:25 -0400

    Starting up react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,15 @@
-afc
+import React from 'react';
+
+function App() {
+  const homePageContent = `
+    <h1>Welcome to My Home Page</h1>
+    <!-- Add your content here -->
+  `;
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: homePageContent }} />
+  );
+}
 
 export default App;
+
