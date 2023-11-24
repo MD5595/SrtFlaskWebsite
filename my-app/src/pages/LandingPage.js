3009d1dc37f20af4e937c851efdd913c6dc0d12b
tolu <toluadegbilero@gmail.com>
2023-11-23 21:02:16 -0500

    Sign in page

--- my-app/src/pages/LandingPage.js
+++ my-app/src/pages/LandingPage.js
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Redirect,Navigate, useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, Redirect, Navigate, useNavigate} from 'react-router-dom';
 import axios from 'axios';
+
 function LandingPage() {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState('');
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
 
 
-  const handleClick = async (e) => {
-    e.preventDefault()
-    try{
-      const response = await axios.post('http://localhost:5000/students', { username });
-      navigate("/HomePage");
-    } catch(error){
-        console.error('Error submitting username:', error);
+    const handleClick = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post('http://localhost:5000/students', {username});
+            navigate("/HomePage");
+        } catch (error) {
+            console.error('Error submitting username:', error);
+        }
+
+    };
+
+    function updateCheckbox(checkbox) {
+        if (checkbox.checked) {
+            checkbox.value = true;
+        } else {
+            checkbox.value = false;
+        }
     }
 
-  };
 
-  return (
-    <div className="container">
-      <form onSubmit={handleClick}>
-        <label htmlFor="name">Username:</label><br />
-        <input type="text" id="name" name="name" value={username}
-               onChange={(e) => setUsername(e.target.value)}/><br />
-        <input type="submit" id="submitButton" value = "Submit" />
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            <form onSubmit={handleClick}>
+                <label htmlFor="name">Username:</label><br/>
+                <input type="text" id="name" name="name" value={username}
+                       onChange={(e) => setUsername(e.target.value)}/><br/>
+                <input type="submit" id="submitButton" value="Submit"/>
+            </form>
+            <label htmlFor="checkAcc">Create New Account</label>
+            <input type="checkbox" id="checkAcc" name="checkAcc" value="False"/>
+
+        </div>
+    );
 }
 
 export default LandingPage;
