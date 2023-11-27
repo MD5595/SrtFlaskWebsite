897b9d9211771f7391d66d92926ec2e113508998
Jiyoo Funai <ashley.funai@ibm.com>
2023-11-27 10:16:14 -0500

    rebased

--- app.py
+++ app.py
@@ -1,10 +1,69 @@
-from flask import Flask, request
 from flask_cors import CORS
 from utils import load_flashcards
 import json
+from flask import Flask, render_template, request, jsonify, session, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='my-app/src')
 cors = CORS(app, supports_credentials=True)
+
+api = Api(app)
+app.config['SECRET_KEY'] = 'Keeey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app_name = 'myapp'
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    username = request.json["username"]
+    existing_user = Students.query.filter_by(username=username).first()
+
+    if existing_user is not None:
+        session['user_id'] = existing_user.id
+        return render_template('home.html')
+    else:
+        new_user = Students(username=username, sessionTime=0, clickAmount=0)
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+
+        return render_template('home.html')
+
+
+@app.route("/addTime", methods=["POST"])
+def addtTime():
+    username = request.json["username"]
+    user = Students.query.filter_by(username=username).first()
+
+
+@app.route('/Students', methods=['GET'])
+def get_students():
+    students = Students.query.all()
+    return jsonify({'Students': [{'username': Students.username} for student in Students]})
+
+@app.route('/students', methods=['POST'])
+def add_student():
+    data = request.get_json()
+    new_username = data.get('username')
+
+    if new_username:
+        class User(db.Model):
+            username = db.Column(db.String, primary_key=True)
+            sessionTime = db.Column(db.Integer, default=0)
+            quizTime = db.Column(db.Integer, default=0)
+            flashcardTime = db.Column(db.Integer, default=0)
+
+        User.__table__.name = f"user_{new_username}"
+
+        new_student = Students(username=new_username)
+        db.session.add(new_student)
+        db.session.commit()
+        return jsonify({'message': 'Student added successfully'}), 201
+    else:
+        return jsonify({'message': 'Invalid data provided'}), 400
+    
 
 @app.route('/get_flashcards', methods=['POST'])
 def get_flashcards():
@@ -28,6 +87,5 @@ def get_units():
     res = json.dumps({'units': response})
     return res
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
