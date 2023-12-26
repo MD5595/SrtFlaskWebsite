f60680e590d92968541c66445a6f8e1173c77c62
tolu <toluadegbilero@gmail.com>
2023-12-26 13:35:18 -0500

    Timer wotk

--- my-app/src/components/Timer.js
+++ my-app/src/components/Timer.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
-function Timer() {
-    const [seconds, setSeconds] = useState(0);
-  return (
 
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+  return (
+    <div>
+      <h1>Time: {seconds} seconds</h1>
+    </div>
   );
 }
 
