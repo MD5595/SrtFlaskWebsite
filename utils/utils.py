c5c1c0365388561215894d0f0e27ef4890a9770f
Jiyoo Funai <ashley.funai@ibm.com>
2024-02-20 10:58:50 -1000

    updated flashcards to include subunits

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,8 @@ def load_flashcards(flashcards_file):
     f = open(flashcards_file, 'r')
     rows = f.readlines()
     for row in rows[1:]:
-        vals = row.split(',')
+        vals = row.split('~')
+        print(vals)
         if vals[0] in flashcards:
             flashcards[vals[0]].append((vals[1],vals[2].strip('\n')))
         else:
