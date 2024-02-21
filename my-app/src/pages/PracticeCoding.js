fc3a85a31e98b4e89b3471121fcc5f1ad873b34a
Tolulope Adegbilero <119755377+MD5595@users.noreply.github.com>
2024-02-21 07:44:13 -0500

    Update PracticeCoding.js

--- my-app/src/pages/PracticeCoding.js
+++ my-app/src/pages/PracticeCoding.js
@@ -3,31 +3,48 @@ import React from 'react';
 function PracticeCoding() {
   return (
     <div className="container">
-      <h1>Welcome!</h1>
-        <p>Unit 1: Introduction <br/>
-        1: Make a program that contains 4 variables. There should be a int, float, string, and true
-            <br/> 
-            2: Make a program that asks the user for their age. Cast the variable that had
+      <h1><a href="https://www.programiz.com/python-programming/online-compiler/"
+             target="_blank">Practice Coding</a>
+</h1>
+        <p><b>Unit 1: Introduction </b><br/>
+        1) Make a program that contains 4 variables. There should be a int, float, string, and true
+            <br/> <br/>
+            2) Make a program that asks the user for their age. Cast the variable that had
             the input assigned to it as a float, then print it.
             <br/>
-            3. Make a program that has 2 variables that contain different numbers.
+            3) Make a program that has 2 variables that contain different numbers.
             Utilize an f-string to print out these numbers with the sentence, "The numbers __ and __ are awesome"
             <br/>
         </p><br/>
-        <p>Unit 2: Math
+        <p><b>Unit 2: Math</b>
         <br/>
-            1: Write a program that utilizes a function to calculate the area on a circle with a radius of 4.
+            1) Write a program that utilizes a function to calculate the area on a circle with a radius of 4.
            <br/>
-            2: Assign -4.25 to a variable, and use an imported function to make the variable's value -4. Use
+            2) Assign -4.25 to a variable, and use an imported function to make the variable's value -4. Use
             another function to make the variable's value 4.
             <br/>
-            3:
+            3)
         </p>
-        <p>Unit 3: Conditionals </p>
-        <p>Unit 4: Loops </p>
-        <p>Unit 5: Functions </p>
-        <p>Unit 6: Lists </p>
-        <a href="https://www.programiz.com/python-programming/online-compiler/" target="_blank">Practice coding</a>
+        <p><b>Unit 3: Conditional</b>
+            <br/>
+1) Make a program that asks the user for their age. If they are 15, have the program "You are allowed to enter" If the user inputs a number other than 15,
+            have the program print "You are not allowed to enter".<br/>
+            2) Make an else if statment that
+            3)
+        </p>
+        <p><b>Unit 4: Loops </b><br/>
+
+        </p>
+        <p><b>Unit 5: Functions</b><br/>
+1) Create a function that has 2 parameters. If the
+        </p>
+        <p><b>.Unit 6: Lists</b><br/>
+1) Create a list that contains one value of each datatype.<br/>
+            2) Create a list that contains 4 values. Have the 3rd index of the list be printed.<br/>
+            3) Write a function that replaces the 4th index of a list with a random value between 0 and 50.<br/>
+            4) Write a function that reverses the order of a list's contents.<br/>
+            5) Write a function that lowercases the first and last strings in a list. <br/>
+        </p>
 
     </div>
   );
