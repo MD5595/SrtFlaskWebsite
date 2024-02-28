53aa97990a3dd1eeff49be71a1657e7cd131986f
tolu <toluadegbilero@gmail.com>
2024-02-28 02:24:14 -0500

    timer location time

--- my-app/src/components/AxiosCalls.js
+++ my-app/src/components/AxiosCalls.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import {baseURL} from "../config.js"
 
 async function send_time(page, username) {
+    const username = localStorage.getItem('username');
+
     var headers = {
         'Content-Type': 'application/json',
       }
@@ -14,3 +16,23 @@ async function send_time(page, username) {
 export {
     send_time
 };
+
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {baseURL} from "../config.js"
+
+function send_time() {
+    const username = localStorage.getItem('username');
+
+    useEffect(() => {
+        axios.post(baseURL + '/sendLocationTime', {page: page, username: username },
+            headers=headers);
+
+
+        return () => clearInterval(timer);
+    }, );
+
+
+}
+
+export default send_time;
