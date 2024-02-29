c4abc80ea5fbf21875e54532a61faa1d38be6de3
tolu <toluadegbilero@gmail.com>
2024-02-29 17:09:36 -0500

    timer

--- my-app/src/components/Timer.js
+++ my-app/src/components/Timer.js
@@ -3,20 +3,25 @@ import axios from 'axios';
 import {baseURL} from "../config.js"
 
 function Timer() {
-        const username = localStorage.getItem('username');
+    const [time, setTime] = useState(0);
+    const [sendTime, setSendTime] = useState(false)
+    const username = localStorage.getItem('username');
 
     var headers = {
         'Content-Type': 'application/json',
       }
-    const [time, setTime] = useState(0);
-    const [sendTime, setSendTime] = useState(false)
+
     if(time>=20){
         setSendTime((sendTime) => !sendTime);
         setTime(0)
     }
-    const timer = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
-    }, 1000);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime((prevTime) => prevTime + 1);
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
 
     useEffect(() => {
         axios.post(baseURL + '/postUserTime', {username:username ,time:time},
